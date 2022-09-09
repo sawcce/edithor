a89ac53e08a59e5e1c17c7f3c2dcf0bc2cacff54
Sawcce <sawcce@proton.me>
2022-09-09 17:11:30 +0200

    Definitions for Input Events in HAL

--- src/hal.h
+++ src/hal.h
@@ -2,3 +2,19 @@ void setup();
 void print(char *data);
 void restore();
 void run();
+
+typedef enum {
+    KeyPressed,
+    KeyReleased 
+} InputEventType;
+
+typedef struct {
+    char character;
+} KeyEvent;
+
+struct InputEvent {
+    InputEventType type;
+    union {
+         KeyEvent keyEvent
+    } event;
+};

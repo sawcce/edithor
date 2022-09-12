47f6eebdee2bf01503744ab7d50fec8da9194ac3
alexis <sawcce@proton.me>
2022-09-12 23:32:18 +0200

    Fixed some issues with format function

--- src/utils.c
+++ src/utils.c
@@ -9,7 +9,9 @@ char *format(char *format, ...) {
   va_list formatArgs;
   va_start(formatArgs, format);
 
-  char *output = malloc(snprintf(NULL, 0, format, formatArgs));
-  sprintf(output, format, formatArgs);
+  int len = snprintf(NULL, 0, format, formatArgs);
+
+  char *output = malloc(len);
+  vsprintf_s(output, len + 1, format, formatArgs);
   return output;
 }
